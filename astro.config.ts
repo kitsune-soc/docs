45c25849cc107f53e121050189d00b63121916a7
aumetra <aumetra@cryptolab.net>
2023-03-29 01:13:02 +0200

    Update domain

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,5 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: `https://kitsune-soc.github.io/docs/`,
-	base: `docs`
+	site: `https://docs.joinkitsune.org`
 });
